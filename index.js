bd57902e3c43e9246ba02cf65d2d5d493aa57ec0
Chris Franklin <cf73021@gmail.com>
2019-01-31 20:10:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,5 +33,8 @@ var sortedRobots = robots.map(function(robot) {
 })
 
 var coloredZebraStripes = zebraStripes.map(function(stripe){
-  if (stripe % 2 === 0)
+  if (stripe % 2 === 0){ return Object.assign({}, stripe, {
+    color: 'black'
+  })}
+  else {return Object.assign({}, stripe, {color: 'white'})}
 })
