2d995e949e6bbfaaab6d04d8afb21e47710f0e5b
Chris Franklin <cf73021@gmail.com>
2019-01-31 20:14:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -35,8 +35,8 @@ var sortedRobots = robots.map(function(robot) {
 var coloredZebraStripes = zebraStripes.map(function(stripe){
   var lastString = stripe.width.toString().slice(-1)
   var lastDigit = parseInt(lastString, 10)
-  
-  if (lastDigit % 2 == 0){ return Object.assign({}, stripe, {
+
+  if (lastDigit % 2 === 0){ return Object.assign({}, stripe, {
     color: 'black'
   })}
   else {return Object.assign({}, stripe, {color: 'white'})}
