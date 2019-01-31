4fac260663d03a0de5fbc0f35356d5f2b2335496
Chris Franklin <cf73021@gmail.com>
2019-01-31 20:15:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ var sortedRobots = robots.map(function(robot) {
   }
 })
 
-var coloredZebraStripes = zebraStripes.map(function(stripe){
+const coloredZebraStripes = zebraStripes.map(function(stripe){
   var lastString = stripe.width.toString().slice(-1)
   var lastDigit = parseInt(lastString, 10)
 
